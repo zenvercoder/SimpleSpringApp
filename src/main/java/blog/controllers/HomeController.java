68d50153295bc0c505d2655047699462a63923ff
Lisa Hull <lisa@jchull.com>
2016-10-29 17:44:06 -0600

    first three posts :sparkles:

--- src/main/java/blog/controllers/HomeController.java
+++ src/main/java/blog/controllers/HomeController.java
@@ -1,12 +1,28 @@
 package blog.controllers;
 
+import blog.models.Post;
+import blog.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class HomeController {
+    // do alt + enter. It's the fix. this time, it imported
+    @Autowired
+    private PostService postService;
+
     @RequestMapping("/")
-    public String index() {
+    public String index(Model model) {
+        List<Post> latest5Posts = postService.findLatest5();
+        model.addAttribute("latest5posts", latest5Posts);
+        List<Post> latest3Posts = latest5Posts.stream()
+                .limit(3).collect(Collectors.toList());
+        model.addAttribute("latest3posts", latest3Posts);
         return "index";
     }
 }
